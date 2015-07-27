b0c07bab751a8f10c507f6e0e593d6cba05c394f
jormis <jore+github@iki.fi>
2015-07-27 18:59:19 +0300

    Till HAS-ALL-THE-AWARDS

--- src/predicates.clj
+++ src/predicates.clj
@@ -13,22 +13,23 @@
   (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
+; empty, nil or contains only whitespace
 (defn blank? [string]
-  :-)
+  (or (== (count (seq string)) 0) (= string nil) (every? whitespace? (seq string))))
 
 (defn has-award? [book award]
-  :-)
+  ((set->predicate (:awards book)) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (set->predicate (:awards book)) awards))
 
 (defn my-some [pred a-seq]
   :-)
@@ -38,4 +39,5 @@
 
 (defn prime? [n]
   :-)
+
 ;^^
